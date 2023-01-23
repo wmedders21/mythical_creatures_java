61f6b9c08b566500a12ef9be849794a720537e4b
Will <wmedders@gmail.com>
2023-01-23 18:59:13 -0500

    Feat: direwolf can have a different name and can have a home

--- src/app/Direwolf.java
+++ src/app/Direwolf.java
@@ -2,12 +2,13 @@ package app;
 
 public class Direwolf {
     public String name;
+    public String home = "Beyond the Wall";
 
     public Direwolf(String name) {
         this.name = name;
     }
 
     public static void main(String[] args) {
-        
+
     }
 }
