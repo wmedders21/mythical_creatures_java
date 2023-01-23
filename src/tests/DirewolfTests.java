793f53fe6ae0a40311326a9dba8af22af20efeb4
Will <wmedders@gmail.com>
2023-01-23 18:45:52 -0500

    Test: direwolf has a name

---
+++ src/tests/DirewolfTests.java
@@ -0,0 +1,13 @@
+package tests;
+
+import app.Direwolf;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DirewolfTests {
+    @Test
+    public void hasAName() {
+        Direwolf direwolf = new Direwolf("Nymeria");
+        assertEquals("Nymeria", direwolf.name);
+    }
