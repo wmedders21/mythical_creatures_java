585b2dd45bdc621835fe7e41d1df56b89851172d
Will Medders <wmedders@gmail.com>
2023-01-24 18:48:01 -0500

    Feat: Dragon

---
+++ src/app/Dragon.java
@@ -0,0 +1,26 @@
+package app;
+
+public class Dragon {
+  public String name;
+  public String color;
+  public String rider;
+  public int eatCount = 0;
+
+  public Dragon(String name, String color, String rider) {
+    this.name = name;
+    this.color = color;
+    this.rider = rider;
+  }
+
+  public Boolean isHungry() {
+    if (eatCount < 3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public void eat() {
+    eatCount++;
+  }
+}
---
+++ src/tests/DragonTests.java
@@ -0,0 +1,62 @@
+package tests;
+import app.Dragon;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Ignore;
+
+public class DragonTests {
+  @Test
+  public void hasAName() {
+    Dragon dragon = new Dragon("Ramoth", "gold", "Lessa");
+
+    assertEquals("Ramoth", dragon.name);
+  }
+
+  @Test
+  public void hasAColor() {
+    Dragon dragon = new Dragon("Ramoth", "gold", "Lessa");
+
+    assertEquals("gold", dragon.color);
+  }
+
+  @Test
+  public void isADifferentDragon() {
+    Dragon dragon = new Dragon("Mnementh", "bronze", "Flar");
+
+    assertEquals("Mnementh", dragon.name);
+  }
+
+  @Test
+  public void hasADifferentColor() {
+    Dragon dragon = new Dragon("Mnementh", "bronze", "Flar");
+
+    assertEquals("bronze", dragon.color);
+  }
+
+  @Test
+  public void hasADifferentRider() {
+    Dragon dragon = new Dragon("Mnementh", "bronze", "Flar");
+
+    assertEquals("Flar", dragon.rider);
+  }
+
+  @Test
+  public void wasBornHungry() {
+    Dragon dragon = new Dragon("Billy", "red", "Sam");
+
+    assertEquals(true, dragon.isHungry());
+  }
+
+  @Test
+  public void eatsALot() {
+    Dragon dragon = new Dragon("Puff", "green", "Sinbad");
+
+    assertEquals(true, dragon.isHungry());
+    dragon.eat();
+    assertEquals(true, dragon.isHungry());
+    dragon.eat();
+    assertEquals(true, dragon.isHungry());
+    dragon.eat();
+    assertEquals(false, dragon.isHungry());
+  }
+}
