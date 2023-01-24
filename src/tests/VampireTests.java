f06e85fc839ff2a09c585f8837b3ea1d39868542
Will <wmedders@gmail.com>
2023-01-24 16:40:08 -0500

    Added all Vampire tests

---
+++ src/tests/VampireTests.java
@@ -0,0 +1,51 @@
+package tests;
+
+import app.Vampire;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class VampireTests {
+    @Test
+    public void hasAName() {
+        Vampire dracula = new Vampire("Dracula");
+
+        assertEquals("Dracula", dracula.name);
+    }
+
+    @Test
+    public void canBeNamedSomethingElse() {
+        Vampire vlad = new Vampire("Vladimir");
+
+        assertEquals("Vladimir", vlad.name);
+    }
+
+    @Test
+    public void keepsAPetBatByDefault() {
+        Vampire ruthven = new Vampire("Ruthven");
+
+        assertEquals("bat", ruthven.pet);
+    }
+
+    @Test
+    public void canKeepOtherPets() {
+        Vampire varney - new Vampire("Varney", "fox");
+
+        assertEquals("fox", varney.pet);
+    }
+
+    @Test
+    public void isThirstyByDefault() {
+        Vampire count = new Vampire("The Count");
+
+        assertEquals(true, count.isThirsty());
+    }
+
+    @Test
+    public void isNotThirstyAfterDrinking() {
+        Vampire liz = new Vampire("Elizabeth Bathory");
+
+        liz.drink();
+
+        assertEquals(false, liz.isThirsty());
+    }
+}
