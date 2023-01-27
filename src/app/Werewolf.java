0d8812a51b0ea740043639d0ecc830ab34b18e96
Will Medders <wmedders@gmail.com>
2023-01-27 11:25:05 -0500

    Feat: werewolf has a name

--- src/app/Werewolf.java
+++ src/app/Werewolf.java
@@ -1,5 +1,12 @@
 package app;
 
 public class Werewolf {
-    
+    public String name;
+
+    public Werewolf(String name) {
+        this.name = name;
+    }
+    public static void main(String[] args) {
+
+    }
 }
