759f79949f122b45c3aeb2fe27c0d41b1ea1afa0
Will Medders <wmedders@gmail.com>
2023-01-27 11:28:01 -0500

    Feat: werewolf has a location

--- src/app/Werewolf.java
+++ src/app/Werewolf.java
@@ -2,9 +2,15 @@ package app;
 
 public class Werewolf {
     public String name;
+    public String location;
 
     public Werewolf(String name) {
         this.name = name;
+    }
+
+    public Werewolf(String name) {
+        this.name = name;
+        this.location = location;
     }
     public static void main(String[] args) {
 
