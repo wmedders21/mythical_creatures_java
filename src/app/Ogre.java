6e31350ad674a93b47057e0ed53b95494ea14863
Will Medders <wmedders@gmail.com>
2023-01-26 22:40:03 -0500

    Feat: ogre lives somewhere by default

--- src/app/Ogre.java
+++ src/app/Ogre.java
@@ -2,8 +2,10 @@ package app;
 
 public class Ogre {
     public String name;
+    public String home;
 
     public Ogre(String name) {
         this.name = name;
+        this.home = "Swamp";
     }
 }
